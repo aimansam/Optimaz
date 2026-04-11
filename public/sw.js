66b63e259bb2700aa3829543ee24faf7a3a1694f
Aiman Sam <aimansammrsm@gmail.com>
2026-04-12 07:58:14 +0800

    fix pwa build error again

---
+++ public/sw.js
@@ -0,0 +1,6 @@
+// This file is required for next-pwa to generate a service worker.
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});

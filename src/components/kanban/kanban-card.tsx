43f1340d736140b807b412b17a1011148ba3e112
Aiman Sam <aimansammrsm@gmail.com>
2026-04-12 11:29:42 +0800

    Revert "edit task kanban"
    This reverts commit 42b2c11aed6e4c92cf38bde9ed11adb808275950.

--- src/components/kanban/kanban-card.tsx
+++ src/components/kanban/kanban-card.tsx
@@ -118,7 +118,7 @@ export function KanbanCard({ task }: { task: Task }) {
 		</div>
 	  </div>
 
-      <Dialog open={editOpen} onClose={() => setEditOpen(false)} title="Edit Task" className="max-w-3xl min-h-100">
+      <Dialog open={editOpen} onClose={() => setEditOpen(false)} title="Edit Task">
         <TaskForm task={task} onClose={() => setEditOpen(false)} />
       </Dialog>
     </>
